8e1c28a2e97a0a19ec6500cb5b35ec20ac76e29b
Gustavo Alves <gustavo_rodrigo@id.uff.br>
2022-01-16 22:07:55 -0300

    finalização do modulo de node

--- nodeJSHCode/index.js
+++ nodeJSHCode/index.js
@@ -1,12 +1,18 @@
 const express = require('express');
 const consign = require('consign');
+const bodyParser = require('body-parser');
+const expressValidator = require('express-validator');
 
 let app = express();
 
-consign().include('routes').into(app);
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(bodyParser.json());
+app.use(expressValidator());
 
-app.listen(3000, '127.0.0.1', () => {
+consign().include('routes').include('utils').into(app);
 
-    console.log("servidor rodando!");
+app.listen(3000, '127.0.0.1', ()=>{
+
+    console.log('servidor rodando!');
 
 });
